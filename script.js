434ec14cebe0aeff293fb362bd382175c242c97d
Liaoxyu <161911756+LiaoxyuCM@users.noreply.github.com>
2025-03-29 13:05:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,3 @@ document.write(text != null ? `<p>${text}</p>`:
     Try to append "?text={The text}", the text valid html and it must be encoded <br>
     <a href="https://anytexteditor.com/cn/url-encoder">Url encoder</a><br><a href="?text=%3Cstyle%3Ebody%20%7Bbackground-color%3A%20%23000%7D%3C%2Fstyle%3E%3Cinput%20type%3D%22text%22%20placeholder%3D%22Type%20something%22%20%2F%3E">Example</a>`
 );
-
