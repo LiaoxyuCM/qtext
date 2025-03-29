09741ffd878acdcb11f721dc52e9e20f0c00ec58
Liaoxyu <161911756+LiaoxyuCM@users.noreply.github.com>
2025-03-29 14:16:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,5 +8,6 @@ const text = getQueryParam('text');
 document.write(text != null ? text :
     `<h1>Welcome to QText!</h1><br>
     Try to append "?text={The text}", the text valid html and it must be encoded <br>
+    <a href="https://www.json.cm/urlencode/">URL encoder</a><br>
     <a href="?text=%3Cstyle%3Ebody%20%7Bbackground-color%3A%20%23000%7D%3C%2Fstyle%3E%3Cinput%20type%3D%22text%22%20placeholder%3D%22Type%20something%22%20%2F%3E">Example</a>`
 );
