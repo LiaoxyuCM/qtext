bd37c3e5fdc214a2efa534b6e9f5ca06dd8525cc
Liaoxyu <161911756+LiaoxyuCM@users.noreply.github.com>
2025-03-29 11:02:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ const getQueryParam = (name) => {
     return params.get(name);
 };
 
-document.write(`<p>${getQueryParam("text")}</p>`);
+const text = getQueryParam('text');
+
+document.write(text != null ? `<p>${text}</p>`:
+    'Try to append "?text={The text}" to my website, the text valid html <br> <a href="https://anytexteditor.com/cn/url-encoder">Url encoder</a><br><a href="https://liaoxyucm.github.io/qtext/?text=%3Cstyle%3Ebody%20%7Bbackground-color%3A%20%23000%7D%3C%2Fstyle%3E%3Cinput%20type%3D%22text%22%20placeholder%3D%22Type%20something%22%20%2F%3E">Example</a>'
+);
