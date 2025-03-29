38d696e326d21f4e288a9da4240ad844a6a0ed71
Liaoxyu <161911756+LiaoxyuCM@users.noreply.github.com>
2025-03-29 12:54:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,10 @@ const getQueryParam = (name) => {
 };
 
 const text = getQueryParam('text');
-const option = getQueryParam('option');
 
-if (option === "blank" | option === "disabled") {
-} else {
-        document.write(text != null ? `<p>${text}</p>`:
-        `<h1>Welcome to QText!</h1><br>
-        Try to append "?text={The text}", the text valid html and it must be encoded <br>
-        <a href="https://anytexteditor.com/cn/url-encoder">Url encoder</a><br><a href="?text=%3Cstyle%3Ebody%20%7Bbackground-color%3A%20%23000%7D%3C%2Fstyle%3E%3Cinput%20type%3D%22text%22%20placeholder%3D%22Type%20something%22%20%2F%3E">Example</a>`
-    );
-}
+document.write(text != null ? `<p>${text}</p>`:
+    `<h1>Welcome to QText!</h1><br>
+    Try to append "?text={The text}", the text valid html and it must be encoded <br>
+    <a href="https://anytexteditor.com/cn/url-encoder">Url encoder</a><br><a href="?text=%3Cstyle%3Ebody%20%7Bbackground-color%3A%20%23000%7D%3C%2Fstyle%3E%3Cinput%20type%3D%22text%22%20placeholder%3D%22Type%20something%22%20%2F%3E">Example</a>`
+);
+
